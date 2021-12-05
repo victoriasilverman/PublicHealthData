ed51051e5f4f3cde77d6226fe2e73fa7ad8442c7
Victoria Silverman <71465807+victoriasilverman@users.noreply.github.com>
2021-12-05 17:01:54 -0500

    Update PHU_Vaxrates_byage.R

--- PHU_Vaxrates_byage.R
+++ PHU_Vaxrates_byage.R
@@ -49,6 +49,24 @@ write.csv(PHU.data.t,"Path to export the DataFrame\\File Name.csv", row.names = 
   ))
   
 
+##want to plot it? Plotting for 5-11 ONLY. 
 
+#create tibble needed
+  PHU.data.g<-as_tibble(PHUdata) %>% #convert it to a tibble
+    select(Date,PHU.name, Agegroup,Percent_at_least_one_dose) %>% #select columns I want
+    mutate(.data=., "Percent_at_least_one_dose" = Percent_at_least_one_dose*100) %>% #create a column converting weight to lbs
+    filter(Date==Today & Agegroup=="05-11yrs") %>% #filter to most recent date
+    select(-Date) %>%  #remove the undisclosed colu
+    arrange(-Percent_at_least_one_dose)
+
+
+#ggplot it UPPPP with some Tswift colours
+ggplot(PHU.data.g) +
+ aes(x = reorder(PHU.name,Percent_at_least_one_dose), weight = Percent_at_least_one_dose) +
+ geom_bar(fill = "#A6836F") + geom_hline(yintercept= c(12.5,25),color='#400303',size=1)+
+ coord_flip() +labs(title="At Least One Dose 5-11 by PHU", x="Public Health Unit",y= "Percentage with ONE dose", subtitle="by Victoria Silverman 2021", caption= "Data from OnGov, vertical lines at 12.5% and 25%")+
+ theme_minimal()+  theme(plot.subtitle=element_text(size=7, hjust=0.5, face="italic", color="black"))+theme(plot.caption=element_text(size=7, hjust=0.5, color="black"))
+
+    
 
 
