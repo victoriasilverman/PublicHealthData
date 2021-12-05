68b03da327f6fe24a775393d42c1ab99262376ce
Victoria Silverman <71465807+victoriasilverman@users.noreply.github.com>
2021-12-05 15:40:56 -0500

    Update PHU_Vaxrates_byage.R

--- PHU_Vaxrates_byage.R
+++ PHU_Vaxrates_byage.R
@@ -1,5 +1,6 @@
 
 library(dplyr)
+library(tidyr)
 library(tidyverse)
 library(formattable)
 
@@ -15,10 +16,12 @@ PHU.data.t<-as_tibble(PHUdata) %>% #convert it to a tibble
   filter(Date==Today) %>% #filter to most recent date
   select(Agegroup,PHU.name,Percent_at_least_one_dose) %>% #select columns I want
   pivot_wider(names_from = Agegroup, values_from = Percent_at_least_one_dose) %>% #pivot it
-  rename("5+" = Ontario_5plus) %>% #rename columns
+  rename("5+" = Ontario_5plus, age5_11="05-11yrs") %>% #rename columns
   select(-c(Undisclosed_or_missing,Adults_18plus,Ontario_12plus)) %>% #remove the undisclosed column
   slice(-35)  %>% #remove the unkonwn PHU row
-  relocate(PHU.name, after="5+")
+  relocate(PHU.name, after="5+")%>%
+  arrange(-age5_11)%>%
+  rename( "05-11yrs"= age5_11)  #rename columns
 
  
   formattable(PHU.data.t, list(
